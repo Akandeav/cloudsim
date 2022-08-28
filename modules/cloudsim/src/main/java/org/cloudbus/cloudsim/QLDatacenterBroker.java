d13d24c0f8cb77407c888c1d57e6b473a7a723db
akandeav <akandevic@gmail.com>
2022-08-28 02:08:16 +0100

    first commit

---
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/QLDatacenterBroker.java
@@ -0,0 +1,177 @@
+package org.cloudbus.cloudsim;
+
+
+import org.cloudbus.cloudsim.*;
+import org.cloudbus.cloudsim.core.CloudSim;
+import org.cloudbus.cloudsim.core.CloudSimTags;
+import org.cloudbus.cloudsim.core.SimEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QLDatacenterBroker extends DatacenterBroker {
+
+    public QLDatacenterBroker(String name) throws Exception {
+        super(name);
+    }
+
+    public void scheduleTaskstoVms() {
+        int reqTasks = cloudletList.size();
+        int reqVms = vmList.size();
+        int maxIndex = 0;
+        int minIndex = 0;
+        float gamma = 0.1f;
+        float alpha = 0.9f;
+        Vm vm = vmList.get(0);
+        float[] qMatrix = new float[reqVms];
+        int[] rewardMatrix = new int[reqVms];
+        Double[] cpuMatrix = new Double[reqVms];
+
+        for(int i = 0; i < reqVms; i++){
+            qMatrix[i] = 0;
+            vm = vmList.get(i);
+            cpuMatrix[i] = vm.getTotalUtilizationOfCpu();
+        }
+        for (int i = 0; i < reqVms; i++){
+            Double temp = cpuMatrix[i];
+            if (temp > cpuMatrix[maxIndex]){
+                maxIndex = i;
+            }
+            if (temp < cpuMatrix[minIndex]){
+                minIndex = i;
+            }
+        }
+        for (int i = 0; i < reqVms; i++){
+            if (i == maxIndex){
+                rewardMatrix[i] = -10;
+            }
+            if (i == minIndex){
+                rewardMatrix[i] = 100;
+            }
+        }
+        for (int i = 0; i < reqVms; i++){
+            qMatrix[i] = (qMatrix[i] * (1 - alpha)) + (alpha * (rewardMatrix[i] + (gamma * qMatrix[maxIndex])));
+        }
+        for (int i = 0; i < reqTasks; i++) {
+            bindCloudletToVm(i, (i % reqVms));
+            System.out.println("Task" + cloudletList.get(i).getCloudletId() + " is bound with VM" + vmList.get(i % reqVms).getId());
+        }
+
+        //System.out.println("reqTasks: "+ reqTasks);
+
+        ArrayList<Cloudlet> list = new ArrayList<Cloudlet>();
+        for (Cloudlet cloudlet : getCloudletReceivedList()) {
+            list.add(cloudlet);
+        }
+
+        //setCloudletReceivedList(null);
+
+        Cloudlet[] list2 = list.toArray(new Cloudlet[list.size()]);
+
+        //System.out.println("size :"+list.size());
+
+        Cloudlet temp = null;
+
+        int n = list.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (list2[j - 1].getCloudletLength() / (vm.getMips() * vm.getNumberOfPes()) > list2[j].getCloudletLength() / (vm.getMips() * vm.getNumberOfPes())) {
+                    //swap the elements!
+                    //swap(list2[j-1], list2[j]);
+                    temp = list2[j - 1];
+                    list2[j - 1] = list2[j];
+                    list2[j] = temp;
+                }
+                // printNumbers(list2);
+            }
+        }
+
+        ArrayList<Cloudlet> list3 = new ArrayList<Cloudlet>();
+
+        for (int i = 0; i < list2.length; i++) {
+            list3.add(list2[i]);
+        }
+        //printNumbers(list);
+
+        setCloudletReceivedList(list);
+
+        //System.out.println("\n\tSJFS Broker Schedules\n");
+        //System.out.println("\n");
+    }
+
+    public void printNumber(Cloudlet[] list) {
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(" " + list[i].getCloudletId());
+            System.out.println(list[i].getCloudletStatusString());
+        }
+        System.out.println();
+    }
+
+    public void printNumbers(ArrayList<Cloudlet> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(" " + list.get(i).getCloudletId());
+        }
+        System.out.println();
+    }
+
+    @Override
+    protected void processCloudletReturn(SimEvent ev) {
+        Cloudlet cloudlet = (Cloudlet) ev.getData();
+        getCloudletReceivedList().add(cloudlet);
+        Log.printLine(CloudSim.clock() + ": " + getName() + ": Cloudlet " + cloudlet.getCloudletId()
+                + " received");
+        cloudletsSubmitted--;
+        if (getCloudletList().size() == 0 && cloudletsSubmitted == 0) {
+            scheduleTaskstoVms();
+            cloudletExecution(cloudlet);
+        }
+    }
+
+    protected void cloudletExecution(Cloudlet cloudlet) {
+
+        if (getCloudletList().size() == 0 && cloudletsSubmitted == 0) { // all cloudlets executed
+            Log.printLine(CloudSim.clock() + ": " + getName() + ": All Cloudlets executed. Finishing...");
+            clearDatacenters();
+            finishExecution();
+        } else { // some cloudlets haven't finished yet
+            if (getCloudletList().size() > 0 && cloudletsSubmitted == 0) {
+                // all the cloudlets sent finished. It means that some bount
+                // cloudlet is waiting its VM be created
+                clearDatacenters();
+                createVmsInDatacenter(0);
+            }
+        }
+    }
+
+    @Override
+    protected void processResourceCharacteristics(SimEvent ev) {
+        DatacenterCharacteristics characteristics = (DatacenterCharacteristics) ev.getData();
+        getDatacenterCharacteristicsList().put(characteristics.getId(), characteristics);
+
+        if (getDatacenterCharacteristicsList().size() == getDatacenterIdsList().size()) {
+            distributeRequestsForNewVmsAcrossDatacenters();
+        }
+    }
+
+    protected void distributeRequestsForNewVmsAcrossDatacenters() {
+        int numberOfVmsAllocated = 0;
+        int i = 0;
+
+        final List<Integer> availableDatacenters = getDatacenterIdsList();
+
+        for (Vm vm : getVmList()) {
+            int datacenterId = availableDatacenters.get(i++ % availableDatacenters.size());
+            String datacenterName = CloudSim.getEntityName(datacenterId);
+
+            if (!getVmsToDatacentersMap().containsKey(vm.getId())) {
+                Log.printLine(CloudSim.clock() + ": " + getName() + ": Trying to Create VM #" + vm.getId() + " in " + datacenterName);
+                sendNow(datacenterId, CloudSimTags.VM_CREATE_ACK, vm);
+                numberOfVmsAllocated++;
+            }
+        }
+
+        setVmsRequested(numberOfVmsAllocated);
+        setVmsAcks(0);
+    }
+}
