0d13ea5786266519fc0db75380b30e0e87f6c7ed
akandeav <akandevic@gmail.com>
2023-10-09 22:31:35 +0100

    feat: response time metric

--- modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/RoundRobinScheduler.java
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/RoundRobinScheduler.java
@@ -150,6 +150,10 @@ public class RoundRobinScheduler {
         double totalCompletionTime=0;
         double totalCost=0;
         double totalWaitingTime=0;
+        double totalResponseTime=0;
+        double minResponseTime=1000000;
+        double maxResponseTime=0;
+        double cloudletSize = 0;
         //-------------------------
         
         DecimalFormat dft = new DecimalFormat("####.##");
@@ -164,6 +168,7 @@ public class RoundRobinScheduler {
                 //HERE:
                 double completionTime= cloudlet.getActualCPUTime()+ cloudlet.getWaitingTime();
                 double cost= cloudlet.getCostPerSec()* cloudlet.getActualCPUTime() ;
+                double responseTime = cloudlet.getFinishTime() - cloudlet.getExecStartTime();
                 //Note: the execution time for a task is cloudlet.getActualCPUTime()
                 //----------------------
             //    Log.printLine(indent + indent + dft.format(cloudlet.getResourceId()) +
@@ -178,6 +183,10 @@ public class RoundRobinScheduler {
                 totalCompletionTime += completionTime;
                 totalCost += cost;
                 totalWaitingTime+=cloudlet.getWaitingTime();
+                totalResponseTime+=responseTime;
+                if (responseTime > maxResponseTime) maxResponseTime = responseTime;
+                if (responseTime < minResponseTime) minResponseTime = responseTime;
+                if (i == 0) cloudletSize = cloudlet.getCloudletOutputSize();
                 //-----------------------------------------
             }
         }
@@ -188,6 +197,11 @@ public class RoundRobinScheduler {
         Log.printLine("Total Completion Time: " + totalCompletionTime +" Avg Completion Time: "+ (totalCompletionTime/size));
         Log.printLine("Total Cost : " + totalCost+ " Avg cost: "+ (totalCost/size));
         Log.printLine("Avg Waiting Time: "+ (totalWaitingTime/size));
+        Log.printLine("Total response Time: " + totalResponseTime);
+        Log.printLine("Avg. response Time: "+ (totalResponseTime/size));
+        Log.printLine("Min. response Time: " + minResponseTime);
+        Log.printLine("Max. response Time: " + maxResponseTime);
+        Log.printLine("Throughput: " + (cloudletSize * size) / totalCompletionTime );
         
     }
 
